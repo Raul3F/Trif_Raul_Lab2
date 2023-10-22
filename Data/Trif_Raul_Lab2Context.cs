ce0b839e76a0d9254dae54b0f6b2d8a1fd7e0c6e
Raul Trif <trif.raul34@gmail.com>
2023-10-22 23:56:29 +0300

    Lab2

---
+++ Data/Trif_Raul_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Trif_Raul_Lab2.Models;
+
+namespace Trif_Raul_Lab2.Data
+{
+    public class Trif_Raul_Lab2Context : DbContext
+    {
+        public Trif_Raul_Lab2Context (DbContextOptions<Trif_Raul_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Trif_Raul_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Trif_Raul_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Trif_Raul_Lab2.Models.Author>? Author { get; set; }
+    }
+}
